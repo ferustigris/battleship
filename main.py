86dd90c3f92b492b337dfc49d4f413e891eda001
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2014-07-15 22:39:13 +0400

    Fields on GUI have been created.

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ kivy.require('1.0.6') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 from kivy.clock import Clock
+
+class CellWidget(Button):
+    pass
 
 class OceanGame(Widget):
     text = StringProperty("Text 1")
@@ -14,6 +18,8 @@ class OceanGame(Widget):
         pass
 #        i += 1
         self.text = "Test 3"
+    def on_touch_down(self, touch):
+        pass
 
 
 class OceanApp(App):
@@ -21,6 +27,10 @@ class OceanApp(App):
     def build(self):
         game = OceanGame()
         Clock.schedule_interval(game.update, 1.0/60.0)
+
+        for i in range(25):
+            game.fieldLeft.add_widget(CellWidget(text="0"))
+            game.fieldRight.add_widget(CellWidget(text="0"))
         return game
 
 
