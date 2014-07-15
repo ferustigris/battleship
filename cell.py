4d38819d85715635517847c4100bc7a668b6e837
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2014-07-15 23:17:50 +0400

    Cell class has been implemented.

---
+++ cell.py
@@ -0,0 +1,4 @@
+class Cell:
+    def __init__(self):
+        self.state = "X"
+
--- main.py
+++ main.py
@@ -6,31 +6,36 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
-from kivy.clock import Clock
+
+from cell import Cell
 
 class CellWidget(Button):
-    pass
+     def __init__(self, cell, **kwargs):
+        super(CellWidget, self).__init__(**kwargs)
+        self.cell = cell
+
+     def onPress(self, e):
+        self.text = self.cell.state
 
 class OceanGame(Widget):
     text = StringProperty("Text 1")
 
-    def update(self, dt):
-        pass
-#        i += 1
-        self.text = "Test 3"
-    def on_touch_down(self, touch):
-        pass
-
+    def __init__(self, cells, **kwargs):
+        super(OceanGame, self).__init__(**kwargs)
+        self.cells = cells
 
 class OceanApp(App):
-
     def build(self):
-        game = OceanGame()
-        Clock.schedule_interval(game.update, 1.0/60.0)
+        cells = []
+        game = OceanGame(cells)
 
-        for i in range(25):
-            game.fieldLeft.add_widget(CellWidget(text="0"))
-            game.fieldRight.add_widget(CellWidget(text="0"))
+        for field in game.fields:
+            for i in range(25):
+                cell = Cell()
+                cells.append(cell)
+                btn1 = CellWidget(cell, text="0")
+                btn1.bind(on_press=btn1.onPress)
+                field.add_widget(btn1)
         return game
 
 
